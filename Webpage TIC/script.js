991baa0f3bad94d05c3d9257f57799d9d1a9af09
Gladras <blackey_bro@yahoo.com>
2016-05-19 22:40:30 +0200

    Add files via upload

---
+++ Webpage TIC/script.js
@@ -0,0 +1,9 @@
+document.getElementById("footer1").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " Grigoriu Cosmin. Site realizat pentru ora de TIC"
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Acasă</a></li>" +
+"<li><a href='turism.html'>Turism</a></li>" +
+"<li><a href='galerie.html'>Galerie</a></li>" +
+"</ul>";
